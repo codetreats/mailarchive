eaea642bd8bd47f201ca5b0c42c09fa076831739
Martin <github@msturm.net>
2023-07-17 19:13:53 +0200

    switch to localhost

--- container/data/prepare.sh
+++ container/data/prepare.sh
@@ -5,26 +5,26 @@ if [ -f /prepared.flag ] ; then
 fi
 
 echo "[PREPARE] Prepare DB"
-mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
-mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "FLUSH PRIVILEGES;"
+mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
+mysql -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "FLUSH PRIVILEGES;"
 
 sleep 30
 
 echo "[PREPARE] Adapt config"
 sed -i 's/thread_stack.*=.*/thread_stack            = 512M/g' /etc/piler/manticore.conf
 sed -i 's/#mailpreviewframe{/#mailpreviewframe{height:400px;/g' /var/piler/www/view/theme/default/assets/css/metro-bootstrap.css
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'INSERT INTO `option` (`key`, `value`) VALUES ("enable_purge", "0");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'INSERT INTO `option` (`key`, `value`) VALUES ("enable_purge", "0");'
 
 
 PASS=$(php hash_password.php $USER_PASSWORD)
 echo "[PREPARE] Adapt piler settings"
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'update user set password = "'"$PASS"'" where uid=0;'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into user (uid, username, realname, password, domain) VALUES ('"$USER_UID"', "'"$USERNAME"'","'"$USERNAME"'","'"$PASS"'","'"$DOMAIN"'");'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into domain (domain, mapped) VALUES ("'"$DOMAIN"'","'"$DOMAIN"'");'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into domain_user (domain, uid) VALUES ("'"$DOMAIN"'",'"$USER_UID"');'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "'"$USERNAME@$DOMAIN"'");'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "x-envelope-to_'"$USERNAME@$DOMAIN"'");'
-mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "x-envelope-from_'"$USERNAME@$DOMAIN"'");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'update user set password = "'"$PASS"'" where uid=0;'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into user (uid, username, realname, password, domain) VALUES ('"$USER_UID"', "'"$USERNAME"'","'"$USERNAME"'","'"$PASS"'","'"$DOMAIN"'");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into domain (domain, mapped) VALUES ("'"$DOMAIN"'","'"$DOMAIN"'");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into domain_user (domain, uid) VALUES ("'"$DOMAIN"'",'"$USER_UID"');'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "'"$USERNAME@$DOMAIN"'");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "x-envelope-to_'"$USERNAME@$DOMAIN"'");'
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into email (uid,email) VALUES ('"$USER_UID"', "x-envelope-from_'"$USERNAME@$DOMAIN"'");'
 touch /prepared.flag
