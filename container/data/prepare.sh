b699e88259fa4a4d48f100d9312f96d0f90b5f6f
Martin <github@msturm.net>
2023-07-17 17:40:38 +0200

    tried some fixes

--- container/data/prepare.sh
+++ container/data/prepare.sh
@@ -9,11 +9,13 @@ mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "
 mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
 mysql -h$MYSQL_HOSTNAME -uroot -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e "FLUSH PRIVILEGES;"
 
+sleep 30
+
 echo "[PREPARE] Adapt config"
 sed -i 's/thread_stack.*=.*/thread_stack            = 512M/g' /etc/piler/manticore.conf
+sed -i 's/#mailpreviewframe{/#mailpreviewframe{height:400px;/g' /var/piler/www/view/theme/default/assets/css/metro-bootstrap.css
 mysql -h$MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE -e 'insert into option (key, value) VALUES ("enable_purge", 0);'
 
-sleep 30
 
 PASS=$(php hash_password.php $USER_PASSWORD)
 echo "[PREPARE] Adapt piler settings"
